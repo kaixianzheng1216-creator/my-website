cf4f2e1eba947483c9e43469956a3d038ba4c558
kaixianzheng <kaixianzheng1216@gmail.com>
2026-06-18 01:41:43 +0800

    feat(homepage): 集成 clsx 库并改进页眉样式类名处理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,12 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import clsx from "clsx";
 
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <header className={styles.heroBanner}>
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
                 <Heading as="h1" className="hero__title">
                     {siteConfig.title}
