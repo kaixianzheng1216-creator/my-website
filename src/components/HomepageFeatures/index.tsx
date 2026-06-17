85d4d09003afee6a1127be6d2890639bc12d4b9a
kaixianzheng <kaixianzheng1216@gmail.com>
2026-06-18 01:12:56 +0800

    style(config): 格式化配置文件代码风格

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,65 +4,65 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: ReactNode;
+    title: string;
+    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: '易于上手',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus 从零开始精心设计，安装简单，帮助你快速搭建并启动网站。
-      </>
-    ),
-  },
-  {
-    title: '专注核心内容',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus 让你专注于文档本身，琐事由我们处理。请将你的文档放入 <code>docs</code> 目录即可。
-      </>
-    ),
-  },
-  {
-    title: '基于 React 构建',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        通过复用 React 组件，你可以扩展或定制网站布局。在扩展功能的同时，仍可复用统一的页眉与页脚。
-      </>
-    ),
-  },
+    {
+        title: '易于上手',
+        Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+        description: (
+            <>
+                Docusaurus 从零开始精心设计，安装简单，帮助你快速搭建并启动网站。
+            </>
+        ),
+    },
+    {
+        title: '专注核心内容',
+        Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+        description: (
+            <>
+                Docusaurus 让你专注于文档本身，琐事由我们处理。请将你的文档放入 <code>docs</code> 目录即可。
+            </>
+        ),
+    },
+    {
+        title: '基于 React 构建',
+        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+        description: (
+            <>
+                通过复用 React 组件，你可以扩展或定制网站布局。在扩展功能的同时，仍可复用统一的页眉与页脚。
+            </>
+        ),
+    },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" aria-label={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} role="img" aria-label={title}/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <Heading as="h3">{title}</Heading>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): ReactNode {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,36 +9,36 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus 教程 - 5分钟 ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <Heading as="h1" className="hero__title">
+                    {siteConfig.title}
+                </Heading>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        Docusaurus 教程 - 5分钟 ⏱️
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`欢迎使用 ${siteConfig.title}`}
-      description="使用 Docusaurus 构建的中文文档站点，帮助你快速创建、管理和发布文档。">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`欢迎使用 ${siteConfig.title}`}
+            description="使用 Docusaurus 构建的中文文档站点，帮助你快速创建、管理和发布文档。">
+            <HomepageHeader/>
+            <main>
+                <HomepageFeatures/>
+            </main>
+        </Layout>
+    );
 }
